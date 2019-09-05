7b0b8627ceb2af42665bbf8607f8b4cca45229cd
weijiahui1 <wjh20140000@163.com>
2019-09-05 15:00:09 +0800

    scratch-l10n 缓存 scratch-l10n-more 本地话走自己的

--- src/reducers/locales.js
+++ src/reducers/locales.js
@@ -1,8 +1,8 @@
 import {addLocaleData} from 'react-intl';
 
-import {localeData} from 'scratch-l10n';
-import editorMessages from 'scratch-l10n/locales/editor-msgs';
-import {isRtl} from 'scratch-l10n';
+import {localeData} from 'scratch-l10n-more';
+import editorMessages from 'scratch-l10n-more/locales/editor-msgs';
+import {isRtl} from 'scratch-l10n-more';
 
 addLocaleData(localeData);
 
